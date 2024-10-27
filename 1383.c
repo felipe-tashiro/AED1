5f9e3fac936af056dd146125829132eff02cc835
felipe-tashiro <felipe.tashiro@unifesp.br>
2024-10-27 09:51:11 -0300

    Update 1383.c

--- 1383.c
+++ 1383.c
@@ -2,30 +2,20 @@
 
 int sudoku[9][9];
 
-int Linha(int m){
+int Verifica(int m){
     int l[10] = {0,0,0,0,0,0,0,0,0,0};
-
+    int c[10] = {0,0,0,0,0,0,0,0,0,0};
+    int b[10] = {0,0,0,0,0,0,0,0,0,0};
+    
     for(int i = 0; i < 9; ++i){
         if(l[sudoku[m][i]])   return 0;
         l[sudoku[m][i]] += 1;
     }
-
-    return 1;
-}
-
-int Coluna(int m){
-    int c[10] = {0,0,0,0,0,0,0,0,0,0};
-
+        
     for(int i = 0; i < 9; ++i){
         if(c[sudoku[i][m]])   return 0;
         c[sudoku[i][m]] += 1;
     }
-
-    return 1;
-}
-
-int Bloco(int m){
-    int b[10] = {0,0,0,0,0,0,0,0,0,0};
 
     for(int i = 0; i < 3; ++i){
         for(int j = 0; j < 3; ++j){
@@ -54,7 +44,7 @@ int main(){
 
         fim = "SIM";
         for(int i = 0; i < 9; ++i){
-            if(Linha(i) == 0 || Coluna(i) == 0 || Bloco(i) == 0){
+            if(Verifica(i) == 0){
                 fim = "NAO";
                 break;
             }

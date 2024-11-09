9e915ef72c5ca6d44b44bb4f892d309a2c79d64c
felipe-tashiro <felipe.tashiro@unifesp.br>
2024-11-08 21:01:46 -0300

    Create 2006.c

---
+++ 2006.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int cha, n , i, total=0;
+    scanf("%d", &cha);
+    for(i=0; i<5; i++)
+    {
+        scanf("%d", &n);
+        if(n==cha) total++;
+    }
+    printf("%d\n", total);
+    return 0;
+}

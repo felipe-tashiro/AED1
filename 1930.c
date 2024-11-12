4029b3dfb3206374dfa5a5107a6d0f3ba8f1f072
felipe-tashiro <felipe.tashiro@unifesp.br>
2024-11-12 13:44:15 -0300

    Create 1930.c

---
+++ 1930.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int T1, T2, T3, T4;
+
+    scanf("%d %d %d %d", &T1, &T2, &T3, &T4);
+
+    int total = T1 + T2 + T3 + T4;
+
+    int tomadas = total - 3;
+
+    printf("%d\n", tomadas);
+
+    return 0;
+}

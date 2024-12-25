3457cc103bf47cf6d957d42884a7533fdc74ac64
felipe-tashiro <felipe.tashiro@unifesp.br>
2024-12-25 14:01:29 -0300

    Create 2791.c

---
+++ 2791.c
@@ -0,0 +1,15 @@
+//2791 - Feijão
+
+#include <stdio.h>
+
+int main() {
+
+    int c1, c2, c3, c4;
+    scanf("%d %d %d %d", &c1, &c2, &c3, &c4);
+
+    int pos = c1*1+c2*2+c3*3+c4*4;
+
+    printf("%d\n", pos);
+
+    return 0;
+}

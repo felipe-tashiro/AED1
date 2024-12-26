d96b34acb8105d78204d44c8ed7ed6fff5a4d54b
felipe-tashiro <felipe.tashiro@unifesp.br>
2024-12-26 14:00:54 -0300

    Create 2752.c

---
+++ 2752.c
@@ -0,0 +1,19 @@
+//2752 - Saída 6
+
+#include <stdio.h>
+
+int main() {
+
+    char str[50] = "AMO FAZER EXERCICIO NO URI";
+
+    printf("<%s>\n", str);
+    printf("<%30s>\n", str);
+    printf("<%.20s>\n", str);
+    printf("<%-20s>\n", str);
+    printf("<%-30s>\n", str);
+    printf("<%.30s>\n", str);
+    printf("<%30.20s>\n", str);
+    printf("<%-30.20s>\n", str);
+
+    return 0;
+}

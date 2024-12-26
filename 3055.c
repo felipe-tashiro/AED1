4585ac7634703b283a811b4be1857dc7576dac80
felipe-tashiro <felipe.tashiro@unifesp.br>
2024-12-26 13:46:45 -0300

    Create 3055.c

---
+++ 3055.c
@@ -0,0 +1,16 @@
+//3055 - Nota Esquecida
+
+#include <stdio.h>
+
+int main() {
+
+    int a, m;
+    scanf("%d", &a);
+    scanf("%d", &m);
+
+    int a2 = (m*2)-a;
+
+    printf("%d\n", a2);
+
+    return 0;
+}

22f0dd0cb6fea10bc80025c66d3199f113a64ade
felipe-tashiro <felipe.tashiro@unifesp.br>
2025-01-03 10:55:58 -0300

    Create 2414.c

---
+++ 2414.c
@@ -0,0 +1,23 @@
+//2414 - Desafio do Maior Número
+
+#include <stdio.h>
+
+int main(){
+
+    int n, maior;
+
+    scanf("%d", &n);
+
+    maior = n;
+
+    while (n != 0){
+        scanf("%d", &n);
+        if (n > maior){
+            maior = n;
+        }
+    }
+
+    printf("%d\n", maior);
+
+    return 0;
+}

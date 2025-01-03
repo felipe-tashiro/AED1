b0afd0744f05b54d6f3801984a3169770774a79b
felipe-tashiro <felipe.tashiro@unifesp.br>
2025-01-03 11:02:04 -0300

    Create 2234.c

---
+++ 2234.c
@@ -0,0 +1,16 @@
+//2234 - Cachorros-Quentes
+
+#include <stdio.h>
+
+int main() {
+
+    float cq, p, media;
+
+    scanf("%d %d", &cq, &p);
+
+    media = cq/p;
+
+    printf("%.2f\n", media);
+
+    return 0;
+}
